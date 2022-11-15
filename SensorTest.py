615d566ef4f9a2aac455525e79efbd877368d065
Rimas <rimantas.smitas@gmail.com>
2022-11-15 16:38:20 +0100

    fixed bugs

--- SensorTest.py
+++ SensorTest.py
@@ -2,31 +2,22 @@ import RPi.GPIO as GPIO
 import time
 from time import sleep
 
-
-
 A = 2
-
-
-
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
 
-
-
 while True:
-   GPIO.setmode(A,GPIO.Out)
+   GPIO.setup(A,GPIO.Out)
    GPIO.output(A,True)
    sleep(0.00001)
-   GPIO.setmode(A,GPIO.In)
+   GPIO.setup(A,GPIO.In)
    timest = time.time_ns()
 
-   while (GPIO.input(5)==True and (time_ns() - timest < 3000));
-   timeCalc = time_ns() - timest 
-    
-   print("Sensor A")
-
-   print(timeCalc)
-   sleep(1)
+   while (GPIO.input(A)==True and (time_ns() - timest < 3000))
+      timeCalc = time_ns() - timest 
+      print("Sensor A")
+      print(timeCalc)
+      sleep(1)
    
