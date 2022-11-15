5c0963240cbba34394917e715a2f064122cc304d
RimantasSmitas <32729788+RimantasSmitas@users.noreply.github.com>
2022-11-15 09:25:16 +0100

    Bugs

--- PWM test.py
+++ PWM test.py
@@ -39,12 +39,12 @@ GPIO.output(DirectionPin1, True)
 
 #start PWM of required Duty Cycle 
 while True:
-    for duty in range(0,101,1):
+    for duty in range(50,101,1):
         pi_pwm.ChangeDutyCycle(duty) #provide duty cycle in the range 0-100
         pi_pwm1.ChangeDutyCycle(duty)
     sleep(0.5)
     
-    for duty in range(100,-1,-1):
+    for duty in range(100,50,-1):
         pi_pwm.ChangeDutyCycle(duty)
         pi_pwm1.ChangeDutyCycle(duty)
     sleep(0.5)
