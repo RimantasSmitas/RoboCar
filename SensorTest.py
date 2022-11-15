86f3a4be406874e00ba12f83f5a4384695ace114
RimantasSmitas <32729788+RimantasSmitas@users.noreply.github.com>
2022-11-15 13:08:02 +0100

    Pull up sensor test

--- SensorTest.py
+++ SensorTest.py
@@ -3,7 +3,6 @@ import time
 
 
 
-
 A = 2
 
 
@@ -19,4 +18,10 @@ GPIO.setup(A, GPIO.IN)
 
 while True:
    print("Sensor A")
+
    print(GPIO.input(A))
+
+
+   GpioPinDigitalInput myButton = gpio.provisionDigitalInputPin(2, PinPullResistance.PULL_UP);
+   print("Sensor Ab")
+   print(myButton)
