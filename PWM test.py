8b6e56b5b24caae8a70a243bb98a5942c8db1d5e
RimantasSmitas <32729788+RimantasSmitas@users.noreply.github.com>
2022-11-15 08:59:51 +0100

    PWM test

---
+++ PWM test.py
@@ -0,0 +1,53 @@
+#PWM test
+
+
+
+
+
+
+import RPi.GPIO as GPIO
+from time import sleep
+
+SpeedPin = 12
+SpeedPin1 = 13
+# PWM pins
+
+DirectionPin = 2
+DirectionPin1 = 3
+
+GPIO.setwarnings(False)			#disable warnings
+GPIO.setmode(GPIO.BOARD)	#set pin numbering system
+
+gpio.cleanup()
+
+GPIO.setup(SpeendPin,GPIO.OUT)
+GPIO.setup(SpeendPin1,GPIO.OUT)
+
+GPIO.setup(DirectionPin,GPIO.OUT)
+GPIO.setup(DirectionPin1,GPIO.OUT)
+
+
+pi_pwm = GPIO.PWM(SpeedPin,1000)		#create PWM instance with frequency
+pi_pwm.start(0)
+
+pi_pwm1 = GPIO.PWM(SpeedPin1,1000)		#create PWM instance with frequency
+pi_pwm1.start(0)			
+
+gpio.output(DirectionPin, True)
+gpio.output(DirectionPin1, True)
+
+
+#start PWM of required Duty Cycle 
+while True:
+    for duty in range(0,101,1):
+        pi_pwm.ChangeDutyCycle(duty) #provide duty cycle in the range 0-100
+        pi_pwm1.ChangeDutyCycle(duty)
+        sleep(0.01)
+    sleep(0.5)
+    
+    for duty in range(100,-1,-1):
+        pi_pwm.ChangeDutyCycle(duty)
+        pi_pwm1.ChangeDutyCycle(duty)
+        
+        sleep(0.01)
+    sleep(0.5)
--- Test.py
+++ Test.py
@@ -1,27 +1,45 @@
+<<<<<<< HEAD
+import RPi.GPIO as gpio
+=======
 import rpi.gpio as gpio
+>>>>>>> 9d4a5ec2fdc1ede680ed4ff3a1a3e90119b5c633
 
 import time
 
 
-gpio.setmode(gpio.BOARD)
+PWMF1 = 2
+PWMF2 = 3
 
-gpio.setup(7, gpio.OUT)
-
-gpio.setup(11, gpio.OUT)
-
-gpio.setup(13, gpio.OUT)
-
-gpio.setup(15, gpio.OUT)
+FrontLeft = 17
+FrontRight = 27
 
 
-gpio.output(7, True)
+gpio.setwarnings(False)
+gpio.cleanup()
 
-gpio.output(11, True)
+gpio.setmode(gpio.BCM)
 
-gpio.output(13, False)
+gpio.setup(PWMF1, gpio.OUT)
+gpio.setup(PWMF2, gpio.OUT)
+gpio.setup(FrontLeft, gpio.OUT)
+gpio.setup(FrontRight, gpio.OUT)
 
-gpio.output(15, False)
+
+gpio.output(PWMF1, True)
+
+gpio.output(FrontLeft, True)
+
+gpio.output(PWMF2, True)
+
+gpio.output(FrontRight, True)
 
 time.sleep(0.5)
 
-gpio.cleanup( )
+gpio.output(FrontLeft, False)
+
+gpio.output(FrontRight, False)
+
+time.sleep(0.5)
+
+gpio.output(PWMF1, False)
+gpio.output(PWMF2, False)
