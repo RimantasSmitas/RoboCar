11759c39fce6c30df8e75e725d2777744776bb84
RimantasSmitas <32729788+RimantasSmitas@users.noreply.github.com>
2022-11-15 09:28:27 +0100

    SleepChange

--- PWM test.py
+++ PWM test.py
@@ -42,8 +42,9 @@ while True:
     for duty in range(50,101,1):
         pi_pwm.ChangeDutyCycle(duty) #provide duty cycle in the range 0-100
         pi_pwm1.ChangeDutyCycle(duty)
-    
+        sleep(0.5)
+                
     for duty in range(100,50,-1):
         pi_pwm.ChangeDutyCycle(duty)
         pi_pwm1.ChangeDutyCycle(duty)
-    sleep(0.5)
+        sleep(0.5)
