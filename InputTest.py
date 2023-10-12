014ca10b18064f5b09459bba85207939b7de9680
Rimas <rism@eadania.dk>
2023-10-12 11:03:44 +0200

    Input test

---
+++ InputTest.py
@@ -0,0 +1,24 @@
+
+def TurnLeft():
+    print("Turning Left")
+
+def TurnRight():
+    print("Turning Right")
+
+def GoForward():
+    print("Going Forward")
+
+def GoBackward():
+    print("reversing")
+
+    
+while True:
+    value = input()
+    if value == "w":
+        GoForward
+    if value == "s":
+        GoBackward
+    if value == "a":
+        TurnLeft
+    if value == "d":
+        TurnRight
